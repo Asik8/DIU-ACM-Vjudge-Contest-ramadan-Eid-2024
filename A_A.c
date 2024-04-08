e2f31b769c9be80fd33b0b5246e5fd0a3d5c272b
Asik8 <motasembillah84@gmail.com>
2024-04-08 12:06:55 +0600

    Another problem solved

---
+++ A_A.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define lli long long int
+#define max_size 100000
+
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int main()
+{
+
+    lli n, count = 0;
+    scanf("%lld", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        count +=i;
+        if(i<n)
+            count--;
+    }
+
+    printf("%lld\n",count);
+    return 0;
+}
